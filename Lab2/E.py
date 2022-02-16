59436ce4b256f65a10368de92669db6a86d0ef72
M1k0sh <jusipbekov.04@gmail.com>
2022-02-17 00:35:13 +0600

    main

---
+++ Lab2/E.py
@@ -0,0 +1,20 @@
+s = input()
+if not ' ' in s:
+    x = int(input())
+    n = int(s)
+else:
+    ind = s.index(' ')
+    n = int(s[:ind])
+    x = int(s[ind:])
+
+a = [0]*n
+res = 0
+for i in range(len(a)):
+    a[i] = x + 2*i
+
+for i in range(len(a)):
+    res = res^a[i]
+
+print(res)
+
+
