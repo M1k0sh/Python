d940c41abe846207ed4ab2e475c0a74d29229a05
M1k0sh <99460001+M1k0sh@users.noreply.github.com>
2022-03-07 19:42:08 +0600

    Update 1.py

--- Lab4/JSON/1.py
+++ Lab4/JSON/1.py
@@ -2,8 +2,16 @@ import json
 
 with open('sample-data.json', "r") as json_file:
     a = json.load(json_file)
-    d = dict(a)
 
-#check
-print(d['totalCount'])
-print(d['imdata'])
+print('Interface Status')
+print("=" * 80)
+print("DN                                                 Description           Speed    MTU")
+print("-------------------------------------------------- --------------------  ------  ------")
+
+imdata = a["imdata"]
+
+for i in range(len(imdata)):
+    for j in imdata[i]:
+        for k in imdata[i][j]:
+            if ("33" in imdata[i][j][k]["dn"]) or ("34" in imdata[i][j][k]["dn"]) or ("35" in imdata[i][j][k]["dn"]):
+                print(f'{imdata[i][j][k]["dn"]}                              {imdata[i][j][k]["speed"]}   {imdata[i][j][k]["mtu"]}') 
